dcafff59b21d8bae2b463f48c3288fc646378ef3
Max Barker <maxbarker26@gmail.com>
2025-02-12 15:37:46 -0900

    Wall Documentation
    Added additional javadoc describing the the generateMazeWalls method.

--- Documents/eclipse-workspace/mud/src/main/java/com/barkerbros/mud/Maze.java
+++ Documents/eclipse-workspace/mud/src/main/java/com/barkerbros/mud/Maze.java
@@ -7,19 +7,33 @@ public class Maze {
 	private Maze maze;
 	private Grid grid;
 	/**
-	 * constructor for generating a new maze
+	 * Constructor for generating a new maze.
 	 * @param size, will generate a maze of size x size cells
 	 */
 	public Maze(int size) {
 		generateMaze(size);
 		
 	}
-	
+	/**
+	 * Generates a grid and walls of a specified size.
+	 * @param size the maximum size of grid and walls, given in constructor
+	 */
 	private void generateMaze(int size) {
 		this.grid = new Grid(size);
 		this.mazeWalls = generateMazeWalls(size);
 		
 	}
+	/**
+	 * Generates walls which form a maze that fits over a grid
+	 * of the size specified in the parameter. The player cannot walk where
+	 * walls objects are generated. The player may walk at any index
+	 * pointing to null.
+	 * @param size the size of the grid which the maze walls will generate
+	 * to fill
+	 * @return Wall[][]	A 2D array the same size as grid, where the indices
+	 * that contain a wall will point to a wall object, and indices that do
+	 * not have a wall will point to null.
+	 */
 	private Wall[][] generateMazeWalls(int size) {
 		Wall[][] walls = new Wall[size][size]; 
 		return walls;
